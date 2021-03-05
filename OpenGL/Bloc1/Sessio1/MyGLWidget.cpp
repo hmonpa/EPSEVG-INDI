6a94abc704a6e736b2d94ce3ca364c00a507dea7
hmonpa <hmonpa@gmail.com>
2021-03-05 10:51:12 +0100

    Bloc1 S1 finished

--- OpenGL/Bloc1/Sessio1/MyGLWidget.cpp
+++ OpenGL/Bloc1/Sessio1/MyGLWidget.cpp
@@ -45,10 +45,11 @@ void MyGLWidget::paintGL ()
   // Activem l'Array a pintar 
   glBindVertexArray(VAO1);
 
-  //glDrawArrays(GL_TRIANGLES, 0, 3)      // Ej 1.1: Número de vértices a pintar!
-  //glDrawArrays(GL_TRIANGLES, 0, 6);     // Ej 1.2
-  glDrawArrays(GL_TRIANGLES, 0, 9);       // Ej 1.3
-
+  //glDrawArrays(GL_TRIANGLES, 0, 3)        // Ej 1.1: Número de vértices a pintar!
+  //glDrawArrays(GL_TRIANGLES, 0, 6);       // Ej 1.2
+  //glDrawArrays(GL_TRIANGLES, 0, 9);       // Ej 1.3
+  //glDrawArrays(GL_TRIANGLES, 0, 12);      // Ej 1.4 y 1.5a)
+    glDrawArrays(GL_TRIANGLES, 0, 24);      // Ej 1.5b)
   // Pintem l'escena
 
 
@@ -87,18 +88,88 @@ void MyGLWidget::creaBuffers1 ()
 
   
     // Exercici 1.3
-    glm::vec3 Vertices[9];  // Nou vèrtexs amb X, Y i Z
+    /*glm::vec3 Vertices[9];  // Nou vèrtexs amb X, Y i Z
     // Parte cuadrada
-    Vertices[0] = glm::vec3(0.5, -0.5, 0.0);
-    Vertices[1] = glm::vec3(0.5, 0.5, 0.0);
-    Vertices[2] = glm::vec3(-0.5, -0.5, 0.0);
-    Vertices[3] = glm::vec3(0.5, 0.5, 0.0);
-    Vertices[4] = glm::vec3(-0.5, 0.5, 0.0);
-    Vertices[5] = glm::vec3(-0.5, -0.5, 0.0);
+    Vertices[0] = glm::vec3(0, -1, 0.0);
+    Vertices[1] = glm::vec3(0, 0, 0.0);
+    Vertices[2] = glm::vec3(-1, -1, 0.0);
+    Vertices[3] = glm::vec3(0, 0, 0.0);
+    Vertices[4] = glm::vec3(-1, 0.0, 0.0);
+    Vertices[5] = glm::vec3(-1, -1, 0.0);
     // Techo
-    Vertices[6] = glm::vec3(0.5, 0.5, 0.0);
-    Vertices[7] = glm::vec3(0.0, 1.0, 0.0);
-    Vertices[8] = glm::vec3(-0.5, 0.5, 0.0);
+    Vertices[6] = glm::vec3(0.0, 0.0, 0.0);
+    Vertices[7] = glm::vec3(-0.5, 1, 0.0);
+    Vertices[8] = glm::vec3(-1, 0.0, 0.0);*/
+
+    // Exercici 1.4
+    /*glm::vec3 Vertices[12];  // Nou vèrtexs amb X, Y i Z
+    // Parte cuadrada
+    Vertices[0] = glm::vec3(0, -1, 0.0);
+    Vertices[1] = glm::vec3(0, 0, 0.0);
+    Vertices[2] = glm::vec3(-1, -1, 0.0);
+    Vertices[3] = glm::vec3(0, 0, 0.0);
+    Vertices[4] = glm::vec3(-1, 0.0, 0.0);
+    Vertices[5] = glm::vec3(-1, -1, 0.0);
+    // Techo 1
+    Vertices[6] = glm::vec3(0.0, 0.0, 0.0);
+    Vertices[7] = glm::vec3(-0.5, 1, 0.0);
+    Vertices[8] = glm::vec3(-1, 0.0, 0.0);
+    // Techo 2
+    Vertices[9] = glm::vec3(0.0, 0.0, 0.0);
+    Vertices[10] = glm::vec3(0.5, 1, 0.0);
+    Vertices[11] = glm::vec3(1, 0.0, 0.0);*/
+
+    // Exercici 1.5a)
+    /*glm::vec3 Vertices[12];  // Nou vèrtexs amb X, Y i Z
+    // Parte cuadrada
+    Vertices[0] = glm::vec3(-1, -0.5, 0.0);
+    Vertices[1] = glm::vec3(-1, -1, 0.0);
+    Vertices[2] = glm::vec3(-0.5, -1, 0.0);
+    Vertices[3] = glm::vec3(-0.5, -1, 0.0);
+    Vertices[4] = glm::vec3(-1, -0.5, 0.0);
+    Vertices[5] = glm::vec3(-0.5, -0.5, 0.0);
+    // Techo 1
+    Vertices[6] = glm::vec3(-1, -0.5, 0.0);
+    Vertices[7] = glm::vec3(-0.5, -0.5, 0.0);
+    Vertices[8] = glm::vec3(-0.75, 0.0, 0.0);
+    // Techo 2
+    Vertices[9] = glm::vec3(-0.5, -0.5, 0.0);
+    Vertices[10] = glm::vec3(0, -0.5, 0.0);
+    Vertices[11] = glm::vec3(-0.25, 0.0, 0.0);*/
+
+    // Exercici 1.5b)
+    glm::vec3 Vertices[24];  // Nou vèrtexs amb X, Y i Z
+    // Parte cuadrada 1
+    Vertices[0] = glm::vec3(-1, -0.5, 0.0);
+    Vertices[1] = glm::vec3(-1, -1, 0.0);
+    Vertices[2] = glm::vec3(-0.5, -1, 0.0);
+    Vertices[3] = glm::vec3(-0.5, -1, 0.0);
+    Vertices[4] = glm::vec3(-1, -0.5, 0.0);
+    Vertices[5] = glm::vec3(-0.5, -0.5, 0.0);
+    // Techo 1
+    Vertices[6] = glm::vec3(-1, -0.5, 0.0);
+    Vertices[7] = glm::vec3(-0.5, -0.5, 0.0);
+    Vertices[8] = glm::vec3(-0.75, 0.0, 0.0);
+    // Techo 2
+    Vertices[9] = glm::vec3(-0.5, -0.5, 0.0);
+    Vertices[10] = glm::vec3(0, -0.5, 0.0);
+    Vertices[11] = glm::vec3(-0.25, 0.0, 0.0);
+
+    // Parte cuadrada 2
+    Vertices[12] = glm::vec3(0.5, 0, 0.0);
+    Vertices[13] = glm::vec3(0.5, 0.5, 0.0);
+    Vertices[14] = glm::vec3(0, 0.5, 0.0);
+    Vertices[15] = glm::vec3(0, 0.5, 0.0);
+    Vertices[16] = glm::vec3(0.5, 0, 0.0);
+    Vertices[17] = glm::vec3(0, 0, 0.0);
+    // Techo 1
+    Vertices[18] = glm::vec3(0.5, 0.5, 0.0);
+    Vertices[19] = glm::vec3(0, 0.5, 0.0);
+    Vertices[20] = glm::vec3(0.25, 1, 0.0);
+    // Techo 2
+    Vertices[21] = glm::vec3(0.5, 0.5, 0.0);
+    Vertices[22] = glm::vec3(1, 0.5, 0.0);
+    Vertices[23] = glm::vec3(0.75, 1, 0.0);
 
   // Creació del Vertex Array Object (VAO) que usarem per pintar
   glGenVertexArrays(1, &VAO1);
