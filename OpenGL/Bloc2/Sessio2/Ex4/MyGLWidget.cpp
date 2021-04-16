7fc6d472c0b732cc18bc736345ef32c24e1cfe74
hmonpa <hmonpa@gmail.com>
2021-04-16 12:13:46 +0200

    Pdte ver suelo rosa

--- OpenGL/Bloc2/Sessio2/Ex4/MyGLWidget.cpp
+++ OpenGL/Bloc2/Sessio2/Ex4/MyGLWidget.cpp
@@ -154,8 +154,8 @@ void MyGLWidget::projectTransform(){
 }
 
 void MyGLWidget::viewTransform(){
-    //glm::mat4 View = glm::lookAt(OBS, VRP, UP);
-    glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, -(radi*1.5)));
+    glm::mat4 View = glm::lookAt(OBS, VRP, UP);
+    //glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, -(radi*1.5)));
     View = glm::translate(View, -VRP);
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &View[0][0]);
 }
