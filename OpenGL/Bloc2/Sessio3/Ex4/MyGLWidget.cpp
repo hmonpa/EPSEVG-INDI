b0564713d552f4f3e66810b740b583e1a9c82cad
hmonpa <hmonpa@gmail.com>
2021-04-24 20:03:48 +0200

    B2 acabado

---
+++ OpenGL/Bloc2/Sessio3/Ex4/MyGLWidget.cpp
@@ -0,0 +1,474 @@
+#include "MyGLWidget.h"
+
+#include <iostream>
+
+MyGLWidget::MyGLWidget (QWidget* parent) : QOpenGLWidget(parent), program(NULL)
+{
+  setFocusPolicy(Qt::StrongFocus);  // per rebre events de teclat
+  scale = 1.0f;
+}
+
+MyGLWidget::~MyGLWidget ()
+{
+  if (program != NULL)
+    delete program;
+}
+
+void MyGLWidget::initializeGL ()
+{
+  // Cal inicialitzar l'ús de les funcions d'OpenGL
+  initializeOpenGLFunctions();
+
+  glClearColor(0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
+  // Activació Z-Buffer
+  glEnable(GL_DEPTH_TEST);
+
+  carregaShaders();
+  creaBuffers();
+  ini_camera();
+}
+
+void MyGLWidget::paintGL ()
+{
+// Aquest codi és necessari únicament per a MACs amb pantalla retina.
+#ifdef __APPLE__
+  GLint vp[4];
+  glGetIntegerv (GL_VIEWPORT, vp);
+  ample = vp[2];
+  alt = vp[3];
+#endif
+
+// En cas de voler canviar els paràmetres del viewport, descomenteu la crida següent i
+// useu els paràmetres que considereu (els que hi ha són els de per defecte)
+//  glViewport (0, 0, ample, alt);
+
+  // Esborrem el frame-buffer a la vegada que el buffer de profunditats
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+  projectTransform();
+  viewTransform();
+
+  // Transformación Patricio
+  modelTransform ();
+  pintaPatri();
+  pintaPatri2();
+  pintaPatri3();
+
+  // Transformación suelo
+  modelTransformTerra();
+  pintaTerra();
+
+  glBindVertexArray (0);
+}
+
+void MyGLWidget::resizeGL (int w, int h)
+{
+  ample = w;    // ample = aw
+  alt = h;      // alt = hw
+
+  raV = float(ample)/float(alt);
+  ra = raV;
+  //std::cout << "Relación aspecto Viewport: " << raV << std::endl;
+  //std::cout << "Relación aspecto Window: " << ra << std::endl;
+
+  // Perspectiva
+  if (raV < 1.0)
+  {
+      FOV = 2.0 * atan(tan(FOV/raV));
+      projectTransform();
+  }
+  else {
+      double alphainicial = FOV/2;
+      double noualpha = (tan(alphainicial)/raV);
+  }
+  std::cout << "FOV: " << FOV << std::endl;
+  // Ortogonal
+  /*if (raV > 1.0 && !decideix)
+  {
+      // Left
+      left = -radi * raV;
+      // Right
+      right = radi * raV;
+      // Bottom
+      bottom = -radi;
+      // Top
+      top = radi;
+      projectTransform();
+  }
+  if (raV > 1.0 && !decideix)
+  {
+      left = -radi;
+      right = radi;
+      bottom = -radi/raV;
+      top = radi/raV;
+      projectTransform();
+  }*/
+
+}
+
+void MyGLWidget::keyPressEvent(QKeyEvent* event)
+{
+  makeCurrent();
+  switch (event->key()) {
+    case Qt::Key_S: { // escalar a més gran
+      scale += 0.05;
+      modelTransform();
+      break;
+    }
+    case Qt::Key_D: { // escalar a més petit
+      scale -= 0.05;
+      modelTransform();
+      break;
+    }
+    case Qt::Key_R: {
+      rota += float(M_PI/4.0);
+      break;
+    }
+  case Qt::Key_O: {
+      decideix=!decideix;   // Positiu (p.def) -> Perspectiva, Negatiu -> Ortogonal
+      projectTransform();
+      break;
+  }
+  case Qt::Key_Z: { // Zoom-in
+      FOV -= (float)M_PI/180;
+      projectTransform();
+      break;
+  }
+  case Qt::Key_X: { // Zoom-out
+      FOV += (float)M_PI/180;
+      projectTransform();
+      break;
+  }
+    default: event->ignore(); break;
+  }
+  update();
+}
+
+void MyGLWidget::creaBuffers ()
+{
+  // Càrrega del objecte Model
+  patri.load("/home/hector/Escritorio/INDI/OpenGL/Bloc2/models/Patricio.obj");
+  calculaCapsaCont();
+  calculaRadiEscena();
+
+  antigaX = Pmin.x;
+  antigaY = Pmin.y;
+
+  carregaPatri();
+  carregaTerra();
+  glBindVertexArray(0);
+}
+
+void MyGLWidget::carregaShaders()
+{
+  // Creem els shaders per al fragment shader i el vertex shader
+  QOpenGLShader fs (QOpenGLShader::Fragment, this);
+  QOpenGLShader vs (QOpenGLShader::Vertex, this);
+  // Carreguem el codi dels fitxers i els compilem
+  fs.compileSourceFile("shaders/basicShader.frag");
+  vs.compileSourceFile("shaders/basicShader.vert");
+  // Creem el program
+  program = new QOpenGLShaderProgram(this);
+  // Li afegim els shaders corresponents
+  program->addShader(&fs);
+  program->addShader(&vs);
+  // Linkem el program
+  program->link();
+  // Indiquem que aquest és el program que volem usar
+  program->bind();
+
+  // Obtenim identificador per a l'atribut “vertex” del vertex shader
+  vertexLoc = glGetAttribLocation (program->programId(), "vertex");
+  // Obtenim identificador per a l'atribut “color” del vertex shader
+  colorLoc = glGetAttribLocation (program->programId(), "color");
+  // Uniform locations
+  transLoc = glGetUniformLocation(program->programId(), "TG");
+  projLoc = glGetUniformLocation(program->programId(), "proj");
+  viewLoc = glGetUniformLocation(program->programId(), "view");
+}
+
+// Project y View
+void MyGLWidget::projectTransform(){
+  // Perspectiva
+  if (decideix){
+      glm::mat4 Proj=glm::perspective(FOV, raV, zN, zF);
+      glUniformMatrix4fv(projLoc, 1, GL_FALSE, &Proj[0][0]);
+  }
+  // Ortogonal
+  /*else
+  {
+      // Left - Right - Bottom - Top - Zn - Zf
+      // Para un viewport cuadrado (raV = 1) -> top = right = -bottom = -top = radi
+      glm::mat4 Proj=glm::ortho(left, right, bottom, top, zN, zF);
+      glUniformMatrix4fv(projLoc, 1, GL_FALSE, &Proj[0][0]);
+  }*/
+
+}
+
+void MyGLWidget::viewTransform(){
+  //glm::mat4 View = glm::lookAt(OBS,VRP,UP);
+  // Versión con ángulos de Euler
+  glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0., 0., -dist));
+  View = glm::rotate(View, tetha, glm::vec3(1,0,0));
+  View = glm::rotate(View, -phi, glm::vec3(0,1,0));
+  View = glm::translate(View, -VRP);
+  glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &View[0][0]);
+}
+
+void MyGLWidget::mousePressEvent(QMouseEvent *e)
+{
+    antigaX = e->x();
+    antigaY = e->y();
+
+    // Sólo se mueve con el botón izquierdo y sin usar modificadores
+    if (e->buttons() == Qt::LeftButton && !(e->modifiers() & (Qt::ShiftModifier | Qt::AltModifier | Qt::ControlModifier))) CurrentAction = ROTATE;
+}
+
+void MyGLWidget::mouseMoveEvent(QMouseEvent *e)
+{
+    if (CurrentAction == ROTATE){
+        float angleX = e->x();
+        float angleY = e->y();
+
+        float x = angleX - antigaX;
+        float y = angleY - antigaY;
+
+        // Pixeles a radianes
+        phi += (x/width())*10;
+        tetha += (y/height())*10;
+
+        viewTransform();
+        update();
+
+        antigaX = angleX;
+        antigaY = angleY;
+    }
+
+}
+
+void MyGLWidget::ini_camera()
+{
+    dist = 2.0 * radi;
+
+    FOV = 2.0 * asin(radi / dist);
+    //FOV = float(M_PI/2.0);
+    ra = 1.0;
+    zN = radi;
+    zF = dist + radi;
+    qDebug("R:%f", radi);
+
+    VRP = glm::vec3(0,0,0);
+    glm::vec3 v = glm::vec3(0.f, 0.25f, 1.f);
+    v = v / length(v);
+    OBS = VRP + dist * v;
+
+    qDebug("OBS:%f,%f,%f", OBS.x, OBS.y, OBS.z);
+    qDebug("VRP:%f,%f,%f", VRP.x, VRP.y, VRP.z);
+    UP = glm::vec3(0,1,0);
+
+}
+
+void MyGLWidget::carregaPatri(){
+    // Creació del Vertex Array Object per pintar
+    glGenVertexArrays(1, &VAO_patri);
+    glBindVertexArray(VAO_patri);
+
+    // Construcció de dos VBOs
+    // 1 - a partir del mètode VBO_vertices()
+    glGenBuffers(1, &VBO_patri);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_patri);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*patri.faces().size()*3*3, patri.VBO_vertices(), GL_STATIC_DRAW);
+
+    // Activem l'atribut vertexLoc
+    glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(vertexLoc);
+
+    // 2 - a partir del mètode VBO_matdiff()
+    glGenBuffers(1, &VBO_patri2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_patri2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*patri.faces().size()*3*3, patri.VBO_matdiff(), GL_STATIC_DRAW);
+
+    // Activem l'atribut colorLoc
+    glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(colorLoc);
+}
+
+void MyGLWidget::pintaPatri3(){
+    // Activem el VAO per a pintar el patri
+    glBindVertexArray (VAO_patri);
+    modelTransform3();
+
+    // pintem
+    glDrawArrays(GL_TRIANGLES, 0, patri.faces().size() * 3);
+}
+
+void MyGLWidget::pintaPatri2(){
+    // Activem el VAO per a pintar el patri
+    glBindVertexArray (VAO_patri);
+    modelTransform2();
+
+    // pintem
+    glDrawArrays(GL_TRIANGLES, 0, patri.faces().size() * 3);
+}
+
+void MyGLWidget::pintaPatri(){
+    // Activem el VAO per a pintar el patri
+    glBindVertexArray (VAO_patri);
+    modelTransform();
+
+    // pintem
+    glDrawArrays(GL_TRIANGLES, 0, patri.faces().size() * 3);
+}
+
+void MyGLWidget::pintaTerra(){
+    // Activación VAO para pintar tierra
+    glBindVertexArray(VAO_terra);
+    // Pintado
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
+
+void MyGLWidget::carregaTerra(){
+    // Primer VBO - posición | Segundo VBO - color
+   glm::vec3 pos_terra[4] = {
+        glm::vec3(-2.5, 0, -2.5),
+        glm::vec3(-2.5, 0, 2.5),
+        glm::vec3(2.5,  0, -2.5),
+        glm::vec3(2.5, 0, 2.5)
+    };
+
+    glm::vec3 col_terra[4] = {
+        glm::vec3(1,0,1),
+        glm::vec3(1,0,1),
+        glm::vec3(1,0,1),
+        glm::vec3(1,0,1)
+    };
+
+    glGenVertexArrays(1, &VAO_terra);
+    glBindVertexArray(VAO_terra);
+
+    glGenBuffers(1, &VBO_terra);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_terra);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(pos_terra), pos_terra, GL_STATIC_DRAW);
+
+    // Activem l'atribut vertexLoc
+    glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(vertexLoc);
+
+    glGenBuffers(1, &VBO_terra2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_terra2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(col_terra), col_terra, GL_STATIC_DRAW);
+
+    // Activem l'atribut colorLoc
+    glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(colorLoc);
+}
+
+// Model transforms ///
+void MyGLWidget::modelTransform3 ()
+{
+    glm::mat4 transform (1.0f);
+    transform = glm::translate(transform, glm::vec3(-2,0,-2));
+    float h = Pmax.y - Pmin.y;
+    rota = M_PI;
+    transform = glm::rotate(transform, rota, glm::vec3(0,1,0));
+    transform = glm::scale(transform, glm::vec3(1/h));
+    transform = glm::translate(transform, -centre);
+    glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::modelTransform2 ()
+{
+    glm::mat4 transform (1.0f);
+    transform = glm::translate(transform, glm::vec3(2,0,2));
+    float h = Pmax.y - Pmin.y;
+
+    //transform = glm::rotate(transform, rota, glm::vec3(0,1,0));
+    transform = glm::scale(transform, glm::vec3(1/h));
+    transform = glm::translate(transform, -centre);
+    glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::modelTransform ()
+{
+  // Matriu de transformació de model
+  glm::mat4 transform (1.0f);
+  float h = Pmax.y - Pmin.y;
+  float hDesitjada = 1;
+  rota = M_PI/2;
+  transform = glm::rotate(transform, rota, glm::vec3(0,1,0));
+  transform = glm::scale(transform, glm::vec3(hDesitjada/h));
+  transform = glm::translate(transform, -centre);
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::modelTransformTerra()
+{
+    // Segona transformació per a que es dibuxi el terra (només matriu identitat)
+      glm::mat4 transform (1.0f);
+      //transform = glm::translate(transform, glm::vec3(0,0,0));
+      //transform = glm::rotate(transform, rota, glm::vec3(0,1,0));
+      glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+
+void MyGLWidget::calculaCapsaCont(){
+    // Pmin de la capsa - inicialització
+    Pmin.x = patri.vertices()[0];
+    Pmin.y = patri.vertices()[1];
+    Pmin.z = patri.vertices()[2];
+
+    // Pmax de la capsa - inicialització
+    Pmax.x = patri.vertices()[0];
+    Pmax.y = patri.vertices()[1];
+    Pmax.z = patri.vertices()[2];
+
+    int mida = patri.vertices().size();
+
+    // Cerca de Pmin i Pmax
+    for (int i=3; i<mida; i+=3)
+    {
+        float nouX = patri.vertices()[i];
+        float nouY = patri.vertices()[i+1];
+        float nouZ = patri.vertices()[i+2];
+
+        // Pmin i Pmax - coordenada x
+        Pmin.x = std::min(Pmin.x, nouX);
+        Pmax.x = std::max(Pmax.x, nouX);
+
+        // Pmin i Pmax - coordenada y
+        Pmin.y = std::min(Pmin.y, nouY);
+        Pmax.y = std::max(Pmax.y, nouY);
+
+        // Pmin i Pmax - coordenada z
+        Pmin.z = std::min(Pmin.z, nouZ);
+        Pmax.z = std::max(Pmax.z, nouZ);
+    }
+    centre = glm::vec3((Pmax.x + Pmin.x)/2, Pmin.y, (Pmax.z + Pmin.z)/2);
+    std::cout << "Capsa contenidora:" << std::endl;
+    std::cout << "Pmin real: (" << Pmin.x << ", " << Pmin.y << ", " << Pmin.z << ")" << std::endl;
+    std::cout << "Pmax real: (" << Pmax.x << ", " << Pmax.y << ", " << Pmax.z << ")" << std::endl << std::endl;
+
+}
+
+void MyGLWidget::calculaRadiEscena()
+{
+    // Pmax y Pmin bien calculados
+    /*centre = glm::vec3((Pmax.x + Pmin.x)/2, (Pmax.y - Pmin.y)/2, (Pmax.z + Pmin.z)/2);
+    float modX = pow(Pmax.x - centre.x, 2);
+    float modY = pow(Pmax.y - centre.y, 2);
+    float modZ = pow(Pmax.z - centre.z, 2);
+
+    radi = sqrt(modX + modY + modZ);*/
+
+    // PMIN y PMAX hardcoder
+    glm::vec3 PMAX = glm::vec3(2.5, 4.0, 2.5);
+    glm::vec3 PMIN = glm::vec3(-2.5, 0.0, -2.5);
+    glm::vec3 centreEscena = glm::vec3((PMAX.x + PMIN.x)/2, (PMAX.y + PMIN.y)/2, (PMAX.z + PMIN.z)/2);
+
+    float modX = pow(PMAX.x - centreEscena.x, 2);
+    float modY = pow(PMAX.y - centreEscena.y, 2);
+    float modZ = pow(PMAX.z - centreEscena.z, 2);
+    radi = sqrt(modX + modY + modZ);
+    std::cout << "Radi: " << radi << std::endl;
+}
+
---
+++ OpenGL/Bloc2/Sessio3/Ex4/MyGLWidget.h
@@ -0,0 +1,93 @@
+#define GLM_FORCE_RADIANS
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include <QKeyEvent>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "/home/hector/Escritorio/INDI/OpenGL/Bloc2/models/Model/model.h"
+
+#include <QMouseEvent>
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ( );
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ( );
+    // resizeGL - És cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);
+    // keyPressEvent - Es cridat quan es prem una tecla
+    virtual void keyPressEvent (QKeyEvent *event);
+
+  private:
+    void creaBuffers ();
+    void carregaShaders ();
+    void modelTransform ();
+    void projectTransform();
+    void viewTransform();
+    void modelTransformTerra();
+    void ini_camera();
+    void pintaPatri();
+    void pintaTerra();
+    void carregaTerra();
+    void carregaPatri();
+
+    void calculaCapsaCont();
+    void calculaRadiEscena();
+
+    void mousePressEvent(QMouseEvent *e);
+    void mouseMoveEvent(QMouseEvent *e);
+
+    typedef enum {ROTATE, NONE, ZOOM} Action;
+    Action CurrentAction;
+
+    void modelTransform2();
+    void modelTransform3();
+    void pintaPatri2();
+    void pintaPatri3();
+
+    // attribute locations
+    GLuint vertexLoc, colorLoc;
+    // uniform locations
+    GLuint transLoc, projLoc, viewLoc;
+    // VAO i VBO names
+    GLuint VAO_patri, VBO_patri, VBO_patri2;    // El primer VBO es para los vértices y el segundo para colores
+    GLuint VAO_terra, VBO_terra, VBO_terra2;
+    // Program
+    QOpenGLShaderProgram *program;
+    // Viewport
+    GLint ample, alt;
+    // Internal vars
+    float scale;
+    glm::vec3 pos;
+
+    Model homer;
+    Model patri;
+    float rota = M_PI/2;
+
+    // Perspectiva
+    float FOV, ra, raV, alphaV, zN, zF;
+    // Ortogonal
+    float left, bottom, right, top;
+
+    glm::vec3 OBS, VRP, UP;
+    glm::vec3 Pmin, Pmax, PMIN, PMAX,  centre;
+    float radi;
+    float dist;
+    bool decideix=true; // Perspectiva=true / Ortgonal=false
+
+    float antigaX, antigaY;
+    float tetha = 1.0, phi = 1.0;
+};
+
